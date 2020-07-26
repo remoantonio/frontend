ca484b22e1d5d98d47741790553cfdeb227a6ecd
Remo Gonzalez <remoantonio@gmail.com>
2020-07-26 17:15:45 -0400

    built

---
+++ build/precache-manifest.ec16cfde5ecd28142e71e6ff1bf520f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc30f26a60ce1bf7232b95a6973dd036",
+    "url": "/index.html"
+  },
+  {
+    "revision": "872ad694c65fb614df19",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e04452bb64ee04e8ba3",
+    "url": "/static/css/main.fa372c8c.chunk.css"
+  },
+  {
+    "revision": "872ad694c65fb614df19",
+    "url": "/static/js/2.d8765082.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d8765082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e04452bb64ee04e8ba3",
+    "url": "/static/js/main.4df2b364.chunk.js"
+  },
+  {
+    "revision": "7cef8c3be03694e81170",
+    "url": "/static/js/runtime-main.f1564d09.js"
+  },
+  {
+    "revision": "b91190cb091f81e6a19cdcf6a9a4be3e",
+    "url": "/static/media/Fork5.b91190cb.jpg"
+  }
+]);
