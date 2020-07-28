cee8a61c3f15a7d32f1801a92b224bf48fdc4e0a
Remo Gonzalez <remoantonio@gmail.com>
2020-07-28 14:23:46 -0400

    dummy cvommit

--- src/components/SavedRecipes.jsx
+++ src/components/SavedRecipes.jsx
@@ -19,6 +19,7 @@ export default class SavedRecipes extends Component {
       headers: myHeaders,
       redirect: 'follow'
     };
+    
 
     fetch(process.env.REACT_APP_BASE_URL + "/fork/savedRecipes", requestOptions)
       .then(response => response.text())
